3b8fe0a2e91b817a60776a77efccf4b1eca232a1
Pranav Ruparelia <pranavruparelia9@gmail.com>
2023-10-24 18:23:45 -0400

    feat: board detailing

---
+++ components/board/todo.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { XCircleIcon } from '@heroicons/react/20/solid';
+import React from 'react'
+import { 
+  DraggableProvidedDragHandleProps, 
+  DraggableProvidedDraggableProps 
+} from 'react-beautiful-dnd';
+
+type TodoCardProps = {
+  todo: Todo;
+  index: number;
+  id: TypedColumn;
+  innerRef: (element: HTMLElement | null) => void;
+  draggableProps: DraggableProvidedDraggableProps;
+  dragHandleProps: DraggableProvidedDragHandleProps | null | undefined;
+}
+
+export const TodoCard = ({
+  todo,
+  dragHandleProps,
+  draggableProps,
+  innerRef
+}: TodoCardProps) => {
+  return (
+    <div
+      {...draggableProps}
+      {...dragHandleProps}
+      ref={innerRef}
+      className='bg-white rounded-md space-y-2 drop-shadow-md'
+    >
+      <div className='flex justify-between items-center p-5'>
+        <p>
+          {todo.title}
+        </p>
+        <button className='text-red-500 hover:text-red-600'>
+          <XCircleIcon className='ml-5 h-8 w-8'/>
+        </button>
+      </div>
+    </div>
+  )
+}
--- components/header/global-search.tsx
+++ components/header/global-search.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { ChangeEvent } from 'react'
 import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
+import { useBoardStore } from '@/store'
 
 export const GlobalSearch = () => {
+  const [searchString, setSearchString] = useBoardStore((state) => [state.searchString, state.setSearchString]);
+
   return (
     <form className='flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial'>
       <MagnifyingGlassIcon className='h-6 w-6 text-gray-400'/>
@@ -9,6 +12,8 @@ export const GlobalSearch = () => {
         type='text' 
         placeholder='Search' 
         className='flex-1 outline-none p-2'
+        value={searchString}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => setSearchString(e.target.value)}
       />
       <button 
         hidden 
